adc080343088437cf41bec26c02f53b3c5ff9037
Haley Serr <haleykpelletier@gmail.com>
2020-02-29 14:03:49 -0600

    SQL Syntax Issues

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
+DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE `burgers` (
-  `id` INTEGER(9) AUTO_INCREMENT NOT NULL
-  ,'burger_name' VARCHAR(250) NOT NULL
-  ,'devoured' BOOLEAN NOT NULL DEFAULT false
-  , PRIMARY KEY ( `id` ) 
+CREATE TABLE burgers (
+  id INTEGER(9) AUTO_INCREMENT NOT NULL
+  , burger_name VARCHAR(250) NOT NULL
+  , devoured BOOLEAN NOT NULL DEFAULT false
+  , PRIMARY KEY ( id ) 
 );
